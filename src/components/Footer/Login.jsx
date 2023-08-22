15c06f1d4177f932583a273455c96c7e84ed6b00
tomas miranda <tmirandafrontan@gmail.com>
2023-08-21 21:44:23 -0300

    cambio login

--- src/components/Footer/Login.jsx
+++ src/components/Footer/Login.jsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Button from '../Button'
 import './Login.css'
 
-export default function Login() {
+function Login() {
+  const [email, setEmail] = useState("");
+  const [isValid, setIsValid] = useState(true);
+  const [isEmailSent, setIsEmailSent] = useState(false);
+
+  const validateEmail = (email) => {
+    const pattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+    return pattern.test(email);
+  }
+
+    const handleEmailChange = (e) => {
+      const newEmail = e.target.value;
+      setEmail(newEmail);
+      setIsValid(validateEmail(newEmail));
+      setIsEmailSent(false);
+    };
+
+    const handleFormSubmit = (e) => {
+      e.preventDefault();
+      if (isValid) {
+        setIsEmailSent(true);
+        setEmail("");
+      }
+    };
+  
   return (
     <div>
         <div className='log-container'>
@@ -10,11 +34,16 @@ export default function Login() {
             <h2>Get early acces today</h2>
             <p>It only take aminute to sign up and our free tier  y extremelly generous if you have any questions, our support team would be happy to help you</p>
          </div>
-         <div className='input-container'>
-            <input type="text" name="" id="" />
+         <form action='#'  onSubmit={handleFormSubmit} className='input-container'>
+            <input value={email} type="email" name="" id="" onChange={handleEmailChange}/>
             <Button text= 'Get started for free'/>
-         </div>
+              {!isValid && (
+              <p className="error">Please enter a valid email address</p>)}
+              {isEmailSent && (
+                <p className="success">¡Submitted successfully!</p>)}
+         </form>
         </div>
     </div>
   )
 }
+export default Login
